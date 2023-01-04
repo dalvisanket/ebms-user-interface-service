6bee3d67bc2f2dcad45ebd8e54514a0a904c89e4
Sanket Dalvi <dalvisanket2010@gmail.com>
2023-01-04 18:45:04 -0500

    #1 : Ui repo setup

---
+++ ui-service/src/main/java/com/ebms/ui_service/mtr_rdng/MeterReadingControllerProxy.java
@@ -0,0 +1,4 @@
+package com.ebms.ui_service.mtr_rdng;
+
+public class MeterReadingControllerProxy {
+}
---
+++ ui-service/src/test/java/com/ebms/ui_service/EbmsUserInterfaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebms.ui_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbmsUserInterfaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
